aa6a2d384ff0dae57c12402679da447db799ca3f
tvnghia <tran.van.nghia@moneyforward.vn>
2022-10-30 13:08:26 +0700

    innit

---
+++ src/containers/home-page/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from 'flowbite-react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button gradientMonochrome="success">Success</Button>
+    </div>
+  )
+}
+
+export default HomePage
